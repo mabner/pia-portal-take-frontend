8f6554b3758ff85664a4bd54e59548585e705bb7
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-22 03:57:00 -0300

    Sets login route to enable testing

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -3,11 +3,13 @@ import { Switch, Route, Redirect } from 'react-router';
 
 import Home from '../components/home/Home';
 import ToolsCrud from '../components/tools/ToolsCrud';
+import Login from './Login';
 
 export default () => (
 	<Switch>
 		<Route exact path="/" component={Home} />
 		<Route path="/tools" component={ToolsCrud} />
+		<Route path="/login" component={Login} />
 		<Redirect from="*" to="/" />
 	</Switch>
 );
