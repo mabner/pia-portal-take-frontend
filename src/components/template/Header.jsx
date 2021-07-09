5a4c453c8bc35c6caa53d38a41876d31a0907361
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-09 01:50:50 -0300

    ads on-page description

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -5,6 +5,9 @@ export default (
 	/** @type {{ icon: any; title: string | number | boolean | {} | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactNodeArray | React.ReactPortal; subtitle: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal; }} */ props
 ) => (
 	<header className="header d-none d-sm-flex flex-column">
+		<meta name="description" content="Portal de Ferramentas Take.
+		 Acesso fácil e rápido as ferramentas disponibilizadas no PowerBI,
+		 API Rest e Jupyter Notebooks em um só lugar." />
 		<h1 className="mt-3">
 			<i className={`fas fa-${props.icon}`}></i> {props.title}
 		</h1>
