332f14d48519b7b876f2a796c8f9afcff9f2e644
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-10 00:26:04 -0300

    useGaTracker test

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -3,13 +3,17 @@ import { Switch, Route, Redirect } from 'react-router';
 
 import Home from '../components/home/Home';
 import ToolsCrud from '../components/tools/ToolsCrud';
+import useGaTracker from '../useGaTracker';
 import Login from './Login';
 
-export default () => (
+export default () =>
+{
+	useGaTracker();
+
 	<Switch>
-		<Route exact path="/" component={Home} />
-		<Route path="/tools" component={ToolsCrud} />
-		<Route path="/login" component={Login} />
+		<Route exact path="/" component={ Home } />
+		<Route path="/tools" component={ ToolsCrud } />
+		<Route path="/login" component={ Login } />
 		{/* <Redirect from="*" to="/" /> */ }
-	</Switch>
-);
+	</Switch>;
+};
---
+++ src/useGaTracker.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+const useGaTracker = () =>
+{
+  const location = useLocation();
+  const [ initialized, setInitialized ] = useState( false );
+
+  useEffect( () =>
+  {
+    if ( !window.location.href.includes( 'localhost' ) )
+    {
+      ReactGA.initialize( 'UA-199987882-1' );
+    }
+    setInitialized( true );
+  }, [] );
+
+  useEffect( () =>
+  {
+    if ( initialized )
+    {
+      ReactGA.pageview( location.pathname + location.search );
+    }
+  }, [ initialized, location ] );
+};
+
+export default useGaTracker;
