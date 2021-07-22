0e7e3580cee5a010c93744d47a26eaceadd3623a
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-22 01:44:50 -0300

    event tracker test

--- src/components/tools/ToolsCrud.jsx
+++ src/components/tools/ToolsCrud.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Main from '../template/Main';
 
 const headerProps = {
@@ -10,55 +10,63 @@ const headerProps = {
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const initialState = {
-	tools: {name: '', description: '', usage: '', tURL: ''},
+	tools: { name: '', description: '', usage: '', tURL: '' },
 	list: [],
 };
-export default class ToolsCrud extends Component {
-	state = {...initialState};
+export default class ToolsCrud extends Component
+{
+	state = { ...initialState };
 
 	// Function will be called when the component is to be shown in the screen
 	componentWillMount ()
 	{
-		axios(BASE_URL).then((resp) => {
-			this.setState({list: resp.data});
-		});
+		axios( BASE_URL ).then( ( resp ) =>
+		{
+			this.setState( { list: resp.data } );
+		} );
 	}
 
 	// Clears the form
-	clear() {
-		this.setState({tools: initialState.tools});
+	clear ()
+	{
+		this.setState( { tools: initialState.tools } );
 	}
 
 	// Saves new tools or alters existing one
-	save() {
+	save ()
+	{
 		const tools = this.state.tools;
 
 		// If tool has valid ID then PUT, no ID do a POST (new record)
 		const method = tools._id ? 'put' : 'post';
 
 		// PUT URL : POST URL
-		const url = tools._id ? `${BASE_URL}/${tools._id}` : BASE_URL;
-		axios[method](url, tools).then((resp) => {
+		const url = tools._id ? `${ BASE_URL }/${ tools._id }` : BASE_URL;
+		axios[ method ]( url, tools ).then( ( resp ) =>
+		{
 			// resp.data returns the data returned from the webservice
-			const list = this.getUpdatedList(resp.data);
-			this.setState({tools: initialState.tools, list});
-		});
+			const list = this.getUpdatedList( resp.data );
+			this.setState( { tools: initialState.tools, list } );
+		} );
 	}
 
-	getUpdatedList(tools, add = true) {
-		const list = this.state.list.filter((t) => t._id !== tools._id);
-		if (add) list.unshift(tools);
+	getUpdatedList ( tools, add = true )
+	{
+		const list = this.state.list.filter( ( t ) => t._id !== tools._id );
+		if ( add ) list.unshift( tools );
 		return list;
 	}
 
-	updateField(event) {
+	updateField ( event )
+	{
 		// Clones the tool with a spread operator
-		const tools = {...this.state.tools};
-		tools[event.target.name] = event.target.value;
-		this.setState({tools});
+		const tools = { ...this.state.tools };
+		tools[ event.target.name ] = event.target.value;
+		this.setState( { tools } );
 	}
 
-	renderForm() {
+	renderForm ()
+	{
 		return (
 			<div className="form">
 				<div className="row">
@@ -69,8 +77,8 @@ export default class ToolsCrud extends Component {
 								type="text"
 								className="form-control"
 								name="name"
-								value={this.state.tools.name}
-								onChange={(event) => this.updateField(event)}
+								value={ this.state.tools.name }
+								onChange={ ( event ) => this.updateField( event ) }
 								placeholder="Digite o nome da ferramenta..."
 							/>
 						</div>
@@ -81,8 +89,8 @@ export default class ToolsCrud extends Component {
 							<textarea
 								className="form-control"
 								name="description"
-								value={this.state.tools.description}
-								onChange={(event) => this.updateField(event)}
+								value={ this.state.tools.description }
+								onChange={ ( event ) => this.updateField( event ) }
 								placeholder="Digite uma descrição para a ferramenta..."
 							/>
 						</div>
@@ -94,8 +102,8 @@ export default class ToolsCrud extends Component {
 								type="text"
 								className="form-control"
 								name="usage"
-								value={this.state.tools.usage}
-								onChange={(event) => this.updateField(event)}
+								value={ this.state.tools.usage }
+								onChange={ ( event ) => this.updateField( event ) }
 								placeholder="Digite o tema da ferramenta..."
 							/>
 						</div>
@@ -107,20 +115,20 @@ export default class ToolsCrud extends Component {
 								type="text"
 								className="form-control"
 								name="tURL"
-								value={this.state.tools.tURL}
-								onChange={(event) => this.updateField(event)}
+								value={ this.state.tools.tURL }
+								onChange={ ( event ) => this.updateField( event ) }
 								placeholder="Digite a URL da ferramenta... https://"
 							/>
 						</div>
 					</div>
 				</div>
-				<hr/>
+				<hr />
 				<div className="row">
 					<div className="col-12 d-flex justify-content-end">
 						<button
 							className="btn btn-success"
 							// @ts-ignore
-							onClick={(event) => this.save(event)}
+							onClick={ ( event ) => this.save( event ) }
 						>
 							Salvar
 						</button>
@@ -128,7 +136,7 @@ export default class ToolsCrud extends Component {
 						<button
 							className="btn btn-secondary ml-2"
 							// @ts-ignore
-							onClick={(event) => this.clear(event)}
+							onClick={ ( event ) => this.clear( event ) }
 						>
 							Cancelar
 						</button>
@@ -139,71 +147,78 @@ export default class ToolsCrud extends Component {
 	}
 
 	// Updates the tools state
-	load(tools) {
-		this.setState({tools});
+	load ( tools )
+	{
+		this.setState( { tools } );
 	}
 
-	remove(tool) {
-		axios.delete(`${BASE_URL}/${tool._id}`).then((_resp) => {
-			const list = this.getUpdatedList(tool, false);
-			this.setState({list});
-		});
+	remove ( tool )
+	{
+		axios.delete( `${ BASE_URL }/${ tool._id }` ).then( ( _resp ) =>
+		{
+			const list = this.getUpdatedList( tool, false );
+			this.setState( { list } );
+		} );
 	}
 
-	renderTable() {
+	renderTable ()
+	{
 		return (
 			<table className="table mt-4">
 				<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Descrição</th>
-					<th>Tema</th>
-					<th>URL</th>
-				</tr>
+					<tr>
+						<th>Nome</th>
+						<th>Descrição</th>
+						<th>Tema</th>
+						<th>URL</th>
+					</tr>
 				</thead>
-				<tbody>{this.renderRow()}</tbody>
+				<tbody>{ this.renderRow() }</tbody>
 			</table>
 		);
 	}
 
-	renderRow() {
-		return this.state.list.map((tools) => {
+	renderRow ()
+	{
+		return this.state.list.map( ( tools ) =>
+		{
 			return (
-				<tr key={tools._id}>
-					<td>{tools.name}</td>
+				<tr key={ tools._id }>
+					<td>{ tools.name }</td>
 					<td>{ tools.description }</td>
-					<td>{tools.usage}</td>
+					<td>{ tools.usage }</td>
 					<td>
 						<div>
-							<a href={tools.tURL} target="_blank" rel="noreferrer">
-								{tools.tURL}
+							<a href={ tools.tURL } target="_blank" rel="noreferrer">
+								{ tools.tURL }
 							</a>
 						</div>
 					</td>
 					<td>
 						<button
 							className="btn btn-warning"
-							onClick={() => this.load(tools)}
+							onClick={ () => this.load( tools ) }
 						>
-							<i className="fas fa-pencil-alt"/>
+							<i className="fas fa-pencil-alt" />
 						</button>
 						<button
 							className="btn btn-danger ml-2"
-							onClick={() => this.remove(tools)}
+							onClick={ () => this.remove( tools ) }
 						>
-							<i className="fas fa-trash"/>
+							<i className="fas fa-trash" />
 						</button>
 					</td>
 				</tr>
 			);
-		});
+		} );
 	}
 
-	render() {
+	render ()
+	{
 		return (
-			<Main {...headerProps}>
-				{this.renderForm()}
-				{this.renderTable()}
+			<Main { ...headerProps }>
+				{ this.renderForm() }
+				{ this.renderTable() }
 			</Main>
 		);
 	}
--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -6,14 +6,16 @@ import ToolsCrud from '../components/tools/ToolsCrud';
 import useGaTracker from '../useGaTracker';
 import Login from './Login';
 
+import withTracker from '../withTracker';
+
 const Routes = () =>
 {
 	useGaTracker();
 	return (
 		<Switch>
-			<Route exact path="/" component={ Home } />
-			<Route path="/tools" component={ ToolsCrud } />
-			<Route path="/login" component={ Login } />
+			<Route exact path="/" component={ withTracker( Home ) } />
+			<Route path="/tools" component={ withTracker( ToolsCrud ) } />
+			<Route path="/login" component={ withTracker( Login ) } />
 			{/* <Redirect from="*" to="/" /> */ }
 		</Switch>
 	);
