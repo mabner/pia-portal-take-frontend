3cab190795729528ca01be65c99dca7590c76afa
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-08-05 23:48:41 -0300

    login name test

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -1,39 +1,47 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
 const BACK_URL = process.env.REACT_APP_BACK_URL;
 
-let cookieValue = document.cookie.replace(
-	/(?:(?:^|.*;\s*)dotcom_user\s*\=\s*([^;]*).*$)|^.*$/,
-	'$1'
-);
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const nav = () => (
-	<aside className="menu-area">
-		<span className="text-warning">Perfil: {cookieValue}</span>
-		<nav className="menu">
-			{/* Refatorar pra componente */}
-			<Link to="/">
-				<i className="fas fa-home" /> Início
-			</Link>
-			<Link to="/tools">
-				<i className="fas fa-tools" /> Cadastro
-			</Link>
-			<Link to="/map">
-				<i className="fas fa-map" /> Mapa
-			</Link>
-			<Link to="/login">
-				<i className="fas fa-sign-in-alt" /> Login
-			</Link>
-			<a
-				href="#"
-				className=""
-				onClick={() => window.open(`${BACK_URL}/logout`, '_top')}
-			>
-				<i className="fas fa-sign-out-alt" /> Logout
-			</a>
-		</nav>
-	</aside>
-);
-export default nav;
+export default class Nav extends Component {
+	componentWillMount() {
+		axios(BASE_URL).then((res) => {
+			this.setState({ list: res.data });
+		});
+	}
+
+	nav = () => (
+		<aside className="menu-area">
+			<span className="text-warning">Perfil: </span>
+			<nav className="menu">
+				{/* Refatorar pra componente */}
+				<Link to="/">
+					<i className="fas fa-home" /> Início
+				</Link>
+				<Link to="/tools">
+					<i className="fas fa-tools" /> Cadastro
+				</Link>
+				<Link to="/map">
+					<i className="fas fa-map" /> Mapa
+				</Link>
+				<Link to="/login">
+					<i className="fas fa-sign-in-alt" /> Login
+				</Link>
+				<a
+					href=""
+					className=""
+					onClick={() => window.open(`${BACK_URL}/logout`, '_top')}
+				>
+					<i className="fas fa-sign-out-alt" /> Logout
+				</a>
+			</nav>
+		</aside>
+	);
+	render() {
+		return this.nav();
+	}
+}
